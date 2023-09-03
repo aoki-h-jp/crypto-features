6b0bd71146e45675fb9d0346d80f2f28804872f0
aoki-h-jp <aoki-h-jp@users.noreply.github.com>
2023-09-03 09:43:16 +0000

    Apply Code Formatter Change

--- crypto_features/feature/information_correlation.py
+++ crypto_features/feature/information_correlation.py
@@ -1,13 +1,14 @@
 """
 Calculate the information correlation.
 """
-import pandas as pd
-import numpy as np
-from scipy.stats import pearsonr
-import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
 import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 from rich import print
+from scipy.stats import pearsonr
+from sklearn.linear_model import LinearRegression
 
 
 class InformationCorrelation:
@@ -19,7 +20,9 @@ class InformationCorrelation:
         pass
 
     @staticmethod
-    def run_calculate(klines: pd.DataFrame, feature: pd.Series, return_minutes=1, **kwargs):
+    def run_calculate(
+        klines: pd.DataFrame, feature: pd.Series, return_minutes=1, **kwargs
+    ):
         """
         Calculate and visualize the information correlation.
 
@@ -35,17 +38,25 @@ class InformationCorrelation:
 
         close_chg_pct_header = f"close_chg_pct_after_{return_minutes}min"
         klines[close_chg_pct_header] = klines["close"].pct_change(return_minutes)
-        klines[close_chg_pct_header] = klines[close_chg_pct_header].shift(-return_minutes)
+        klines[close_chg_pct_header] = klines[close_chg_pct_header].shift(
+            -return_minutes
+        )
         klines[close_chg_pct_header] = klines[close_chg_pct_header].fillna(0)
-        klines[close_chg_pct_header] = klines[close_chg_pct_header].replace([np.inf, -np.inf, np.nan, -np.nan], 0)
+        klines[close_chg_pct_header] = klines[close_chg_pct_header].replace(
+            [np.inf, -np.inf, np.nan, -np.nan], 0
+        )
         klines[close_chg_pct_header] = klines[close_chg_pct_header].astype(float)
         klines[close_chg_pct_header] = klines[close_chg_pct_header].round(4)
 
         feature.index = feature.index.map(lambda x: x.replace(microsecond=0))
         feature_arr = feature[feature.index.isin(klines.index)].values
-        klines_arr = klines[klines.index.isin(feature.index)][close_chg_pct_header].values
+        klines_arr = klines[klines.index.isin(feature.index)][
+            close_chg_pct_header
+        ].values
 
-        assert len(feature_arr) == len(klines_arr), f"len(feature_arr)={len(feature_arr)}, len(klines_arr)={len(klines_arr)}"
+        assert len(feature_arr) == len(
+            klines_arr
+        ), f"len(feature_arr)={len(feature_arr)}, len(klines_arr)={len(klines_arr)}"
 
         print("[green] Start calculating the information correlation... [/green]")
 
@@ -59,10 +70,18 @@ class InformationCorrelation:
 
         # Visualize
         plt.scatter(feature_arr, klines_arr * 100)
-        plt.plot(feature_arr, lr.predict(feature_arr.reshape(-1, 1)) * 100, color='red', linewidth=1, linestyle='-.')
+        plt.plot(
+            feature_arr,
+            lr.predict(feature_arr.reshape(-1, 1)) * 100,
+            color="red",
+            linewidth=1,
+            linestyle="-.",
+        )
         plt.xlabel(feature.name)
         plt.ylabel("close_chg_pct [%]")
-        plt.title(f"rho={round(rho, 3)}, pval={round(pval, 3)}\ncoef={round(lr.coef_[0][0], 3)}, intercept={round(lr.intercept_[0], 3)}\n{feature.name} vs close_chg_pct_after_{return_minutes}min")
+        plt.title(
+            f"rho={round(rho, 3)}, pval={round(pval, 3)}\ncoef={round(lr.coef_[0][0], 3)}, intercept={round(lr.intercept_[0], 3)}\n{feature.name} vs close_chg_pct_after_{return_minutes}min"
+        )
         plt.tight_layout()
         save_dir = f"information_correlation/{feature.name}_vs_close_chg_pct_after_{return_minutes}min.png"
         if kwargs.get("save_name", False):
