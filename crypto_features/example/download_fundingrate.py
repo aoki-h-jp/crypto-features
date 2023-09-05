5648b068f807247e1d059fa55540ffbb304d2c96
Hirotaka Aoki <113173839+aoki-h-jp@users.noreply.github.com>
2023-09-06 08:07:34 +0900

    Add example

---
+++ crypto_features/example/download_fundingrate.py
@@ -0,0 +1,4 @@
+from crypto_features.download.download_fundingrate import BinanceFundingRateDownload, BybitFundingRateDownload
+
+BinanceFundingRateDownload().run_download()
+BybitFundingRateDownload().run_download()
---
+++ crypto_features/example/download_liquidationsnapshot.py
@@ -0,0 +1,3 @@
+from crypto_features.download.download_liquidationsnapshot import BinanceLiquidationSnapshotDownload
+
+BinanceLiquidationSnapshotDownload().run_download()
